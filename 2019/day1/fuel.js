2e7b00ce57f9821d893f99cf07757f5a1cd78d5a
Bhanuteja <145745009+Bhanuteja74@users.noreply.github.com>
2025-02-02 11:48:22 +0530

    day-1

---
+++ 2019/day1/fuel.js
@@ -0,0 +1,34 @@
+const input = () => {
+  const data = Deno.readTextFileSync("./2019/day1/input.txt");
+  return data.split("\n");
+};
+
+const fuelRequired = (mass) => Math.floor(mass / 3) - 2;
+const add = (a, b) => a + b;
+
+const part1 = (masses) => {
+  const fuelForEachMass = masses.map(fuelRequired);
+  return fuelForEachMass.reduce(add, 0);
+};
+
+const fuelRequiredIncludesFuelMass = (mass, totalFuel = 0) => {
+  const fuel = fuelRequired(mass);
+
+  if (fuel < 1) return totalFuel;
+  return fuelRequiredIncludesFuelMass(fuel, totalFuel + fuel);
+};
+
+const part2 = (masses) => {
+  const fuelForEachMass = masses.map(fuelRequiredIncludesFuelMass);
+  return fuelForEachMass.reduce((a, b) => a + b, 0);
+};
+
+const main = () => {
+  const masses = input();
+
+  console.log("Day-1");
+  console.log(" part-1", part1(masses));
+  console.log(" part-2", part2(masses));
+};
+
+main();
