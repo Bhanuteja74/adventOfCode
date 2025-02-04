e41339462c6b63b55bbabfcd76631e6d91d0d82e
Bhanuteja <145745009+Bhanuteja74@users.noreply.github.com>
2025-02-04 15:20:22 +0530

    day-2 part2

--- 2019/day2/intcode.js
+++ 2019/day2/intcode.js
@@ -13,7 +13,7 @@ const mul = (data, index) => {
   return data;
 };
 
-const part1 = (intcode) => {
+const part1 = ([...intcode]) => {
   const operation = { 1: add, 2: mul };
   let index = 0;
 
@@ -22,14 +22,29 @@ const part1 = (intcode) => {
     operation[intcode[index]](intcode, index);
     index += 4;
   }
+
   return intcode[0];
+};
+
+const part2 = ([...intcode]) => {
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      const newIntcode = [...intcode];
+      newIntcode[1] = noun;
+      newIntcode[2] = verb;
+      if (part1(newIntcode) === 19690720) return 100 * noun + verb;
+    }
+  }
+
+  return 0;
 };
 
 const main = () => {
   const intcode = input();
   intcode[1] = 12;
   intcode[2] = 2;
-  console.log(part1(intcode));
+  console.log("part-1", part1(intcode));
+  console.log("part-2", part2(intcode));
 };
 
 main();
